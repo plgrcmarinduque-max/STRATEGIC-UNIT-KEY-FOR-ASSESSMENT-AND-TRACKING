8644039e85cf7676a06c3592d6847995bb4f0d00
Urinox <francisdimailig98@gmail.com>
2026-03-14 14:49:55 +0800

    Update

--- src/main.jsx
+++ src/main.jsx
@@ -1,5 +1,6 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
+import { Analytics } from "@vercel/analytics/next"
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import "./index.css";
 
